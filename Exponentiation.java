6e920bc72d77b689917a60fd83a074eb60e5bb19
AvraamOvolono <avraamovolono@yandex.ru>
2023-02-26 20:41:04 +0300

    Add clean data method to Exponentiation project

--- Exponentiation.java
+++ Exponentiation.java
@@ -1,6 +1,37 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Exponentiation {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
         
+    }
+
+    // Считывание информации с файла
+    public static String read(String path) throws FileNotFoundException, IOException{
+        String data = "";
+        BufferedReader br = new BufferedReader(new FileReader(path));
+        try {
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+
+            while (line != null) {
+                sb.append(line);
+                sb.append(System.lineSeparator());
+                line = br.readLine();
+            }
+            data = sb.toString();
+        }
+        finally {
+            br.close();
+        }
+        return data;
+    }
+
+    // Убираем все лишнее, оставляем символы и буквы
+    public static String clean(String data) {
+        return data.replaceAll("[^\\w+]", "");
     }
 }
 
