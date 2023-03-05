f1f7ba853851f7cc0dfddccfc9ed6de804b26b3f
AvraamOvolono <avraamovolono@yandex.ru>
2023-03-05 16:07:36 +0300

    Add create array method

--- MergeSort.java
+++ MergeSort.java
@@ -2,6 +2,11 @@ public class MergeSort {
     public static void main(String[] args) {
         
     }
+
+    public static int[] createArray(int size) {
+        int[] array = new int[size];
+        return array;
+    }
 }
 
 // 1. Создание массива
