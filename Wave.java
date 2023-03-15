8516db316b2fb3bbc4756db4ade8850de7c7ad4f
AvraamOvolono <avraamovolono@yandex.ru>
2023-03-16 00:06:42 +0300

    Add decomp comments

--- Wave.java
+++ Wave.java
@@ -1,5 +1,12 @@
 public class Wave {
     public static void main(String[] args) {
-        
+
     }
 }
+
+// 1. Генератор карты getMap
+// 2. Печать карты mapToString
+// 3. Получение точки getPoint
+// 4. Волновой алгоритм
+// 4.1 Очередь в четырех направлениях
+// 4.2 Заполнение пустых клеток
