0ef19d666db52171659977edc7cb8a6808be721b
AvraamOvolono <avraamovolono@yandex.ru>
2023-03-20 16:59:57 +0300

    Wave algorithm added

--- Wave.java
+++ Wave.java
@@ -43,39 +43,39 @@ public class Wave {
     //     return new int[] { x, y };
     // }
 
-    public static void wave(int[][] map, int[] startPos) {
+    // public static void wave(int[][] map, int[] startPos) {
 
-        Queue<int[]> queue = new LinkedList<int[]>();
-        queue.add(startPos);
-        map[startPos[0]][startPos[1]] = 1;
+    //     Queue<int[]> queue = new LinkedList<int[]>();
+    //     queue.add(startPos);
+    //     map[startPos[0]][startPos[1]] = 1;
 
-        while (queue.size() != 0) {
-            int[] position = queue.remove();
-            int posX = position[0];
-            int posY = position[1];
+    //     while (queue.size() != 0) {
+    //         int[] position = queue.remove();
+    //         int posX = position[0];
+    //         int posY = position[1];
 
-            if (map[posX - 1][posY] == 0) {
-                int[] temp = new int[] { posX - 1, posY };
-                queue.add(temp);
-                map[posX - 1][posY] = map[posX][posY] + 1;
-            }
-            if (map[posX][posY - 1] == 0) {
-                int[] temp = new int[] { posX, posY - 1 };
-                queue.add(temp);
-                map[posX][posY - 1] = map[posX][posY] + 1;
-            }
-            if (map[posX + 1][posY] == 0) {
-                int[] temp = new int[] { posX + 1, posY };
-                queue.add(temp);
-                map[posX + 1][posY] = map[posX][posY] + 1;
-            }
-            if (map[posX][posY + 1] == 0) {
-                int[] temp = new int[] { posX, posY + 1 };
-                queue.add(temp);
-                map[posX][posY + 1] = map[posX][posY] + 1;
-            }
-        }
-    }
+    //         if (map[posX - 1][posY] == 0) {
+    //             int[] temp = new int[] { posX - 1, posY };
+    //             queue.add(temp);
+    //             map[posX - 1][posY] = map[posX][posY] + 1;
+    //         }
+    //         if (map[posX][posY - 1] == 0) {
+    //             int[] temp = new int[] { posX, posY - 1 };
+    //             queue.add(temp);
+    //             map[posX][posY - 1] = map[posX][posY] + 1;
+    //         }
+    //         if (map[posX + 1][posY] == 0) {
+    //             int[] temp = new int[] { posX + 1, posY };
+    //             queue.add(temp);
+    //             map[posX + 1][posY] = map[posX][posY] + 1;
+    //         }
+    //         if (map[posX][posY + 1] == 0) {
+    //             int[] temp = new int[] { posX, posY + 1 };
+    //             queue.add(temp);
+    //             map[posX][posY + 1] = map[posX][posY] + 1;
+    //         }
+    //     }
+    // }
 }
 
 class Point2D {
@@ -153,6 +153,41 @@ class MapPrinter {
         }
 
         return sb.toString();
+    }
+}
+
+class WaveAlgorithm {
+    int[][] map;
+    
+    public WaveAlgorithm(int[][] map) {
+        this.map = map;
+    }
+
+    public void waves(Point2D startPoint) {
+        Queue<Point2D> queue = new LinkedList<Point2D>();
+        queue.add(startPoint);
+        map[startPoint.x][startPoint.y] = 1;
+
+        while (queue.size() != 0) {
+            Point2D pos = queue.remove();
+
+            if (map[pos.x - 1][pos.y] == 0) {
+                queue.add(new Point2D(pos.x - 1, pos.y));
+                map[pos.x - 1][pos.y] = map[pos.x][pos.y] + 1;
+            }
+            if (map[pos.x][pos.y - 1] == 0) {
+                queue.add(new Point2D(pos.x, pos.y - 1));
+                map[pos.x][pos.y - 1] = map[pos.x][pos.y] + 1;
+            }
+            if (map[pos.x + 1][pos.y] == 0) {
+                queue.add(new Point2D(pos.x + 1, pos.y));
+                map[pos.x + 1][pos.y] = map[pos.x][pos.y] + 1;
+            }
+            if (map[pos.x][pos.y + 1] == 0) {
+                queue.add(new Point2D(pos.x, pos.y + 1));
+                map[pos.x][pos.y + 1] = map[pos.x][pos.y] + 1;
+            }
+        }
     }
 }
 
